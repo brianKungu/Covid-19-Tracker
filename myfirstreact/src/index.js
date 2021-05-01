a3c8d6962ba199bf346ddfed67d3f584cf8cc381
Your Name <you@example.com>
2021-05-01 21:57:58 +0300

    Covid-19 Tracker

---
+++ myfirstreact/src/index.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import 'bootstrap/dist/css/bootstrap.css';
+// // import Counter from './components/counter';
+// import "./App";
+
+// const element=<h1>Hello World</h1>;
+// ReactDOM.render(<Counter/>, document.getElementById('root'));
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
